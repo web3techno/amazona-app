c16550f8f22c9fa0a6fe10db46094ee29a9c54a3
web3techno <web3techservice@gmail.com>
2023-03-11 14:17:01 +0530

    create sample users in mongo DB

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+    users: [
+      {
+        name: 'John',
+        email: 'admin@example.com',
+        password: bcrypt.hashSync('1234', 8),
+        isAdmin: true
+      },
+      {
+        name: 'Peter',
+        email: 'user@example.com',
+        password: bcrypt.hashSync('1234', 8),
+        isAdmin: false
+      }
+    ],
     products: [
       {
         _id: '1',
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import data from './data.js';
+import userRouter from './routers/userRouter.js';
 
 const app = express();
-
-app.get('/', (req, res) => {
-    res.send('Server is ready');
-});
-
-app.get('/api/products', (req, res) => {
-    res.send(data.products);
+mongoose.connect(process.env.MONGODB_URL || "mongodb+srv://web3tech:web3tech@cluster0.z4br9eo.mongodb.net/amazona", {
+    useNewUrlParser: true, 
+    // useCreateIndex : true, 
+    useUnifiedTopology : true 
 });
 
 app.get('/api/products/:id', (req, res) => {
@@ -19,6 +18,20 @@ app.get('/api/products/:id', (req, res) => {
         res.status(404).send({message: "Product not found"});
     }
 });
+
+app.get('/api/products', (req, res) => {
+    res.send(data.products);
+});
+
+app.use('/api/users', userRouter);
+
+app.get('/', (req, res) => {
+    res.send('Server is ready');
+});
+
+app.use((err, req, res, next) => {
+    res.status(500).send({message: err.message});
+})
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
