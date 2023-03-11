f0c7c346cf712fcd8274faf988200cfce0a7d01f
web3techno <web3techservice@gmail.com>
2023-03-11 15:04:39 +0530

    design sign in screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import ProductScreen from './screens/ProductScreen';
 import HomeScreen from './screens/HomeScreen';
 import CartScreen from './screens/CartScreen';
 import { useSelector } from 'react-redux';
+import SigninScreen from './screens/SigninScreen';
 
 function App() {
   const cart = useSelector(state => state.cart);
@@ -28,6 +29,7 @@ function App() {
           <Routes>
             <Route path="/cart/:id?" element={<CartScreen/>} exact></Route>
             <Route path="/product/:id" element={<ProductScreen/>} exact></Route>
+            <Route path="/signin" element={<SigninScreen/>}></Route>
             <Route path="/" element={<HomeScreen/>}></Route>
           </Routes>
         </main>
