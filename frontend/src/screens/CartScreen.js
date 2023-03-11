964d30dfd2e8a675d97d4db27a6c6b709855a913
web3techno <web3techservice@gmail.com>
2023-03-11 12:09:11 +0530

    implement add to cart button

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux';
 import { useLocation, useParams } from 'react-router-dom';
+import { addToCart } from '../actions/cartActions';
 
 export default function CartScreen() {
     const params = useParams();
@@ -7,6 +9,12 @@ export default function CartScreen() {
     const { search } = useLocation();
     const qtyInUrl = new URLSearchParams(search).get('qty');
     const qty = qtyInUrl ? Number(qtyInUrl) : 1;
+    const dispatch = useDispatch();
+    useEffect(() => {
+        if(productId) {
+            dispatch(addToCart(productId, qty));
+        }
+    }, [dispatch, productId, qty]);
     return (
         <div>
             <h1>Cart Screen</h1>
