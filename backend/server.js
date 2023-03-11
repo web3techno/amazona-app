fe7720169daf87d2dae21ae61efdc67a377f796e
web3techno <web3techservice@gmail.com>
2023-03-11 11:00:56 +0530

    add redux to home screen

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import data from './data.js';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Server is ready');
+});
+
+app.get('/api/products', (req, res) => {
+    res.send(data.products);
+});
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log('Served at http://localhost:' + port);
+}); 
