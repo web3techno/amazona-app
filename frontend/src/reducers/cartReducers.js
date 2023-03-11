ba276ab91002e4485c3abf18a2d3b4d15bfa2ea1
web3techno <web3techservice@gmail.com>
2023-03-11 12:32:08 +0530

    build cart screen

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -4,9 +4,9 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
     switch(action.type) {
         case CART_ADD_ITEM :
             const item = action.payload;
-            const existItem = state.cartItems.find(x => x.product === item.product);
+            const existItem = state.cartItems.find((x) => x.product === item.product);
             if (existItem) {
-                return {...state, cartItems: state.cartItems.map(x => cartReducer.product === existItem.product ? item : x)}
+                return {...state, cartItems: state.cartItems.map((x) => x.product === existItem.product ? item : x)}
             } else {
                 return {...state, cartItems: [...state.cartItems, item]};
             }
